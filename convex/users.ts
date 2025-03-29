d8cdcf386556aeebef2da44e446c3406ebb6f8d5
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-03-29 11:31:49 +0530

    Add user query functionality and implement Header component

--- convex/users.ts
+++ convex/users.ts
@@ -1,5 +1,5 @@
 import { v } from "convex/values"
-import {mutation} from "./_generated/server"
+import {mutation,query} from "./_generated/server"
 
 export const syncUser =mutation({
     args:{
@@ -8,7 +8,7 @@ export const syncUser =mutation({
         name:v.string(),
     },
 
-    handler:async (convexToJson,args)=>{
+    handler:async (ctx,args)=>{
         const existingUser = await ctx.db
       .query("users")
       .filter((q) => q.eq(q.field("userId"), args.userId))
@@ -23,5 +23,22 @@ if (!existingUser){
     })
 }
        
+    }
+})
+
+export const getUser= query({
+    args:{userId: v.string()},
+
+    handler:async (ctx,args)=>{
+      if(!args.userId) return null;
+      
+      const user =await ctx.db
+      .query("users")
+      .withIndex("by_user_id")
+      .filter((q)=>q.eq(q.field("userId"),args.userId))
+      .first();
+
+      if(!user) return null;
+      return user;
     }
 })
---
+++ src/app/(root)/_components/Header.tsx
@@ -0,0 +1,17 @@
+import { currentUser } from "@clerk/nextjs/server";
+import {ConvexHttpClient} from "convex/browser"
+
+
+async function Header() {
+
+    const convex =new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+    const user =await currentUser();
+
+
+
+    
+    return <div>Header</div>
+
+    
+}
+export default Header
