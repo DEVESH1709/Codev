c199c85f33a52041a48be7c984acd281d0c59f3f
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-03-30 19:56:24 +0530

    Implement copy functionality in OutputPanel and manage copied state

--- src/app/(root)/_components/OutputPanel.tsx
+++ src/app/(root)/_components/OutputPanel.tsx
@@ -1,7 +1,26 @@
 "use client"
 
+import { useCodeEditorStore } from "@/store/useCodeEditorStore";
+import { useState } from "react";
+
 function OutputPanel(){
-    return <div>Output</div>
+  const [output,error,isRunning ]= useCodeEditorStore();
+  const [isCopied,setIsCopied]= useState(false)
+
+  const hasContent =error || output
+
+  const handleCopy = async ()=>{
+    if(!hasContent()) return 
+    await navigator.clipboard.write(error || output)
+    setIsCopied(true)
+
+    setTimeout(()=>{
+        setIsCopied(false);
+    },2000);
+  }
+
+  return 
+  
 }
 
 export default OutputPanel;
