69cb1df6891b304108460c75c89f8da52024d7dc
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-03-31 12:01:21 +0530

    Add queries for retrieving snippets and checking if snippets are starred

--- convex/snippets.ts
+++ convex/snippets.ts
@@ -1,4 +1,4 @@
-import { mutation } from "./_generated/server";
+import { mutation,query } from "./_generated/server";
 import {v} from "convex/values"
 
 export const createSnippets =mutation({
@@ -32,3 +32,30 @@ handler: async (ctx,args)=>{
     })
 },
 });
+
+
+export const getSnippets= query({
+    handler:async(ctx)=>{
+        const snippets =await ctx.db.query("snippets").order("desc");
+        return snippets;
+
+    }
+})
+
+export const isSnippetsStarred = query({
+    args:{
+        snippetsId:v.id("snippets")
+    },
+    handler:async(ctx,args)=>{
+        const identity= await ctx.auth.getUserIdentity();
+        if(!identity) return false;
+
+        const star = await ctx.db
+        .query("stars")
+        .withIndex("by_user_id_and_snippets_id")
+        .filter(
+            (q)=>q.eq(q.field("userId"),identity.subject) && q.eq(q.field("snippetsId"),args.snippetId)).first()
+    return !!star;
+    
+        }
+})
