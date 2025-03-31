23c865b27849de64918df6a0b290ee6947797561
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-03-31 20:14:55 +0530

    Add delete functionality to SnippetCard component

--- src/app/snippets/_components/SnippetCard.tsx
+++ src/app/snippets/_components/SnippetCard.tsx
@@ -1,14 +1,26 @@
 
-
+"use client";
 import {Snippet} from"@/types"
+import { useUser } from "@clerk/nextjs"
+import { useMutation } from "convex/react";
+
+import { api } from "../../../../convex/_generated/api";
+
+import {motion} from "framer-motion"
+
+function SnippetCard({snippet}:{snippet:Snippet}){
+  const {user}=useUser();
 
 
+   const deleteSnippe =useMutation(api.snippets.deleteSnippet)
+  const [isDeleting,setDeleting] =usetate(false);
+  
+  const handleDelete =async()=>{
 
+  }
 
+  return 
 
-function SnippetCard((snippet):{snippet:Snippet}){
-
-
-    return <div>SnippetCard</div>
+    
 }
 export default SnippetCard;
