23e626e918ae31ecf21fb5fb0cbda438dd49eb5e
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-01 16:19:22 +0530

    Rename SnippetLoadSkeleton to SnippetLoadingSkeleton and create new loading skeleton component

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -4,7 +4,7 @@ function page() {
     const snippetId =useParams().id;
  cosnt snippet =useQuery(api.snippets.getSnippetById,{snippetId: snippetId a Id<"snippets">});
 
-if(snippet === undefined ) return <SnippetLoadSkeleton></SnippetLoadSkeleton>
+if(snippet === undefined ) return <SnippetLoadingSkeleton></SnippetLoadingSkeleton>
 
 
   return (
