5fb4be89a5eb0e541b61468d63581f5a9d7a4fd2
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-01 17:07:28 +0530

    Add CopyButton component for copying code to clipboard with feedback

---
+++ src/app/snippets/[id]/_components/CopyBuuton.tsx
@@ -0,0 +1,31 @@
+"use client"
+import { Check,Copy } from "lucide-react";
+import { useState } from "react";
+function CopyBuuton ({code}:{code:string}) {
+
+    const [copied, setCopied] =useState(false);
+
+    const copyToClipboard =async()=>{
+        await navigator.clipboard.writeText(code);
+        setCopied(true);
+        setTimeout(()=>setCopied(false),2000)
+    }
+  return (
+   <button
+   onClick={copyToClipboard}
+   className=" p-2 hover:bg-white/10 rounded-bg transition-all duration-200 group relative"
+   >
+ {copied ?(
+    <Check className="size-4 text-green-400"></Check>
+ ):(
+    <Copy className="size-4 text-gray-400 group-hover:text-gray-300">
+
+    </Copy>
+ )
+
+ }
+   </button>
+  )
+}
+
+export default CopyBuuton
