b1f95e35da94a8a256990a3cead2ff2872325343
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-01 17:15:18 +0530

    Add Comments component to display comments for snippets

---
+++ src/app/snippets/[id]/_components/Comments.tsx
@@ -0,0 +1,8 @@
+import { Id } from "../../../../../convex/_generated/dataModel";
+
+
+function Comments({snippetId}:{anippetsId:Id<"snippets">}){
+    return <div>Comments</div>
+
+}
+export default Comments;
