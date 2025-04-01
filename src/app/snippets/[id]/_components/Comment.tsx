591a82ffe031a121bb979717096df05ca46c36d0
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-01 22:04:31 +0530

    Refactor Comment component to include TypeScript props for better type safety and clarity

--- src/app/snippets/[id]/_components/Comment.tsx
+++ src/app/snippets/[id]/_components/Comment.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+interface CommentProps{
+    comment:{
+        _id:Id<"snippetComments">;
+        _creationTime:number;
+        userId:string;
+        userName:string;
+        snippetId :Id<"snippets">;
+        content:string;
+    };
 
+    onDelete:(commentId:Id<"snippettComments">)=> void;
+        isDeleting :boolean;
+        currentUserrId?:string;
+    
+}
 
-function Comment() {
+function Comment({comment,currentUserId, isDeleting, onDelete} :CommentProps) {
   return (
     <div>
       
