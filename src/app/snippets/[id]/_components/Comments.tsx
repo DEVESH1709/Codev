c86c6c0b32e198f1e877bf6119d3a86d8ebb060a
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-01 17:52:54 +0530

    Implement comment submission and deletion functionality in Comments component

--- src/app/snippets/[id]/_components/Comments.tsx
+++ src/app/snippets/[id]/_components/Comments.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
 import { Id } from "../../../../../convex/_generated/dataModel";
+import { api } from "../../../../../convex/_generated/api";
+import toast from "react-hot-toast";
 
 
 function Comments({snippetId}:{anippetsId:Id<"snippets">}){
+    const user =useUser();
+    const {isSubmitting, setIsSubmitting} = useState(false);
+    const [deleteCommentId,setDeleteCommentId] =useState<string |null>(null);
+    
+    const comments =useQuery(api.snippets.getComments,(snippetId));
+    const addComment = useMutation (api.snippets.addComment);
+    const deleteComment = useMutation(api.snippets.deleteComment);
+  
+const handleSubmitComment =async(content:string)=>{
+ setIsSubmitting(true);
+
+ try{
+await addComment({snippetId,content})
+ }catch(error){
+    console.log("Error adding comment:",error);
+    toast.error("Something went wrong ")
+ } finally{
+    setIsSubmitting(false);
+ }
+}
+const handleDeleteComment =async(commentId:Id <"snippetComments">)=>{
+  setDeleteCommentId(commentId);
+
+  try{
+await deleteComment({commentId});
+  }catch(error){
+    console.log("Error deleting comment:",error);
+   toast.error("Something went wrong");
+
+  }finally{
+    setDeleteCommentId(null);
+  }
+}
+
     return <div>Comments</div>
 
 }
