c3273c00eae625b455f9104379ebab4fe3448f57
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-02 20:31:01 +0530

    Update UpgradeButton component to link to the checkout URL for user upgrades

--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -2,9 +2,10 @@ import { Zap } from "lucide-react";
 import Link from "next/link"
 
 export default function UpgradeButton(){
+    const CHECKOUT_URL = "https://codev-store.lemonsqueezy.com/buy/3d2749b1-db03-4de2-b449-b1a304e40715"
     return(
         <Link
-           href={"/"}
+           href={ CHECKOUT_URL}
            className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg hiver:from-blue-600 hover:to-blue-700 transition-all"
         >
             <Zap className="w-5 h-5"></Zap>
