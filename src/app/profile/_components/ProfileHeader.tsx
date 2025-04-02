eeff3526b09fadb2c1976f616724ac9689ab25ac
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-02 11:37:44 +0530

    Add ProfileHeader component with TypeScript interface for props

---
+++ src/app/profile/_components/ProfileHeader.tsx
@@ -0,0 +1,15 @@
+interface ProfileHeaderProps{
+    userStats:any,
+    userData:any,
+    user:any,
+}
+
+function ProfileHeader({userStats,userData,user}:ProfileHeaderProps) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileHeader
