b098767517d9a6beb839ccb17657ebd0a2cde202
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-04-02 12:40:29 +0530

    Add FeatureCategory and FeatureItem components skeletons

---
+++ src/app/pricing/_components/FeatureCategory.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeatureCategory() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeatureCategory
---
+++ src/app/pricing/_components/FeatureItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FeatureItem() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default FeatureItem
